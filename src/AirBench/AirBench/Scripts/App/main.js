1e23be37b6c4771cf30e9f055956038a2dcbb527
James Churchill <james@smashdev.com>
2019-10-07 10:01:11 -0700

    Add bench list filtering

--- src/AirBench/AirBench/Scripts/App/main.js
+++ src/AirBench/AirBench/Scripts/App/main.js
@@ -1,8 +1,30 @@
 ﻿
 (async () => {
-  const benches = await airBenchData.getBenches();
-  console.log(benches);
+  const benchesDivId = 'benches';
+  const benchFilteringId = 'bench-filtering';
+  const minNumberOfSeatsInputId = 'minNumberOfSeats';
+  const maxNumberOfSeatsInputId = 'maxNumberOfSeats';
 
-  const benchesDiv = document.getElementById('benches');
+  const benchesDiv = document.getElementById(benchesDivId);
+  const benchFiltering = document.getElementById(benchFilteringId);
+  const minNumberOfSeatsInput = document.getElementById(minNumberOfSeatsInputId);
+  const maxNumberOfSeatsInput = document.getElementById(maxNumberOfSeatsInputId);
+
+  const benches = await airBenchData.getBenches();
+
   benchesDiv.innerHTML = airBenchBenchesHtml.getTableHtml(benches);
+
+  benchFiltering.addEventListener('keyup', (event) => {
+    const minNumberOfSeats = parseInt(minNumberOfSeatsInput.value, 10);
+    const maxNumberOfSeats = parseInt(maxNumberOfSeatsInput.value, 10);
+
+    const filteredBenches = benches.filter(bench => 
+      (isNaN(minNumberOfSeats) && isNaN(maxNumberOfSeats)) ||
+      (isNaN(minNumberOfSeats) && bench.numberOfSeats <= maxNumberOfSeats) ||
+      (isNaN(maxNumberOfSeats) && bench.numberOfSeats >= minNumberOfSeats) ||
+      (bench.numberOfSeats >= minNumberOfSeats && bench.numberOfSeats <= maxNumberOfSeats)
+    );
+
+    benchesDiv.innerHTML = airBenchBenchesHtml.getTableHtml(filteredBenches);
+  });
 })();
